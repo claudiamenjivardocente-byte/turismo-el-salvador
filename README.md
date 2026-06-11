0f4098d02ca483808a78b56a9e83af2edceec250
claudiamenjivardocente-byte <claudia.menjivar.docente@clases.edu.sv>
2026-06-11 14:47:15 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,38 @@
+\# Turismo El Salvador
+
+Landing page interactiva desarrollada con HTML, CSS y JavaScript.
+
+
+
+\## Características
+
+\- Navbar fija
+
+\- Menú hamburguesa
+
+\- Hero
+
+\- Servicios
+
+\- Formulario validado
+
+\- Manipulación del DOM
+
+\- Diseño responsive
+
+
+
+\## Tecnologías
+
+\- HTML5
+
+\- CSS3
+
+\- JavaScript
+
+
+
+\## Autor
+
+Claudia Stefani Menjívar Lara
+
---
+++ script.js
@@ -0,0 +1,48 @@
+const formulario = document.getElementById("formulario");
+const respuesta = document.getElementById("respuesta");
+
+formulario.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    let nombre = document.getElementById("nombre").value;
+    let correo = document.getElementById("correo").value;
+    let telefono = document.getElementById("telefono").value;
+
+    if(nombre.length < 3){
+        respuesta.textContent =
+        "Ingrese un nombre válido";
+        return;
+    }
+
+    if(!correo.includes("@")){
+        respuesta.textContent =
+        "Ingrese un correo válido";
+        return;
+    }
+
+    if(telefono.length < 8){
+        respuesta.textContent =
+        "Ingrese un teléfono válido";
+        return;
+    }
+
+    respuesta.textContent =
+    "Gracias por contactarnos. Te responderemos pronto.";
+
+});
+
+
+
+const boton = document.getElementById("hamburguesa");
+const menu = document.getElementById("menu");
+
+boton.addEventListener("click", ()=>{
+
+    if(menu.style.display === "flex"){
+        menu.style.display = "none";
+    }else{
+        menu.style.display = "flex";
+    }
+
+});
